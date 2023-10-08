33f048f69e9d02b8002473cd4215633b98d33d02
muhzinkhan <muhzin029@gmail.com>
2023-10-08 20:04:49 +0530

    Add email alert

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Amazon Price Tracker
 
-This script takes an amazon product URL and parses it for the price listed
+- This script takes an amazon product URL and parses it for the price listed.
+
+- It sends an email if the amazon listed price is less than the `buyprice` you entered.
--- main.py
+++ main.py
@@ -1,26 +1,53 @@
 import requests
+import os
+import dotenv
 import lxml
 from bs4 import BeautifulSoup
 import smtplib
 
-URL = "https://www.amazon.in/dp/B0BDJ6ZMCC"
+dotenv.load_dotenv()
+
+YOUR_SMTP_ADDRESS = os.getenv("YOUR_SMTP_ADDRESS")
+YOUR_EMAIL = os.getenv("YOUR_EMAIL")
+YOUR_PASSWORD = os.getenv("YOUR_PASSWORD")
+User_Agent = os.getenv("User-Agent")
+
+
+URL = "https://www.amazon.in/dp/B0BDJ6ZMCC"     # Enter the link here
+BUY_PRICE = 120000                              # Enter the buy price here
+
 
 header = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
+    "User-Agent": User_Agent,
     "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
 }
 
 response = requests.get(URL, headers=header)
 
 print(response)
-# print(response.text)
 
 soup = BeautifulSoup(response.text, 'lxml')
 # print(soup.prettify())
 
 price = soup.find(class_="a-price-whole").get_text()
+title = soup.find(id="productTitle").get_text().strip()
 
-print(price)
-# price_without_comma = price.split(",")
-# print(price_without_comma)
-# print(price_without_comma[0], price_without_comma[1])
+price_concat = ""
+for i in price.split(","):
+    price_concat += i
+
+price_float = float(price_concat)
+print(price_float)
+
+
+if price_float <= BUY_PRICE:
+    message = f"{title} is now {price}"
+
+    with smtplib.SMTP(YOUR_SMTP_ADDRESS, port=587) as connection:
+        connection.starttls()
+        result = connection.login(YOUR_EMAIL, YOUR_PASSWORD)
+        connection.sendmail(
+            from_addr=YOUR_EMAIL,
+            to_addrs=YOUR_EMAIL,
+            msg=f"Subject:Amazon Price Alert!\n\n{message}\n{URL}".encode("utf-8")
+        )
